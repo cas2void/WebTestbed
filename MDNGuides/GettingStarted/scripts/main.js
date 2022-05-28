1802356a01f40d6343f90cb9557c043947eb797a
cas2void <cas2void@gmail.com>
2022-05-28 11:40:33 +0800

    Convert to tabs

--- MDNGuides/GettingStarted/scripts/main.js
+++ MDNGuides/GettingStarted/scripts/main.js
@@ -1,36 +1,36 @@
 const myImage = document.querySelector('img')
 const originalWidth = myImage.style.width
 myImage.onmouseenter = function () {
-    myImage.style.width = '510px'
+	myImage.style.width = '510px'
 }
 myImage.onmouseleave = () => {
-    myImage.style.width = originalWidth
+	myImage.style.width = originalWidth
 }
 
 const myButton = document.querySelector('button')
 const myHeading = document.querySelector('h1')
 
 function setHeading(name) {
-    myHeading.textContent = "Node is Cool, " + name
+	myHeading.textContent = "Node is Cool, " + name
 }
 
 function setUserName() {
-    const myName = prompt('Please enter you name')
-    if (!myName) {
-        setUserName()
-    } else {
-        localStorage.setItem('name', myName)
-        setHeading(myName)
-    }
+	const myName = prompt('Please enter you name')
+	if (!myName) {
+		setUserName()
+	} else {
+		localStorage.setItem('name', myName)
+		setHeading(myName)
+	}
 }
 
 myButton.onclick = () => {
-    setUserName();
+	setUserName();
 }
 
 const storedName = localStorage.getItem('name')
 if (!storedName) {
-    setUserName()
+	setUserName()
 } else {
-    setHeading(storedName)
+	setHeading(storedName)
 }
